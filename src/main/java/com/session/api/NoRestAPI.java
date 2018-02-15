e8e7a9f0fb474976ba7a441f5da6c42765e8efdc
Prasanna <vprasana@thoughtworks.com>
2018-02-15 20:35:37 +0530

    RMM Level 0 api

--- src/main/java/com/session/api/NoRestAPI.java
+++ src/main/java/com/session/api/NoRestAPI.java
@@ -1,6 +1,10 @@
 package com.session.api;
 
+import org.springframework.http.HttpRequest;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,10 +12,30 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class NoRestAPI {
-    
+
     @GetMapping("/hello")
     public String helloWorld() {
         return "Hello World !!!";
     }
 
+    public String printName() {
+        return "Name is Prasanna";
+    }
+
+    public String sessionName() {
+        return "REST API Documentation";
+    }
+
+    @PostMapping("/invoke")
+    public String isItARestMethod(@RequestBody String methodName) {
+        methodName = methodName.trim();
+        System.out.println("Method name is "+methodName);
+        if (methodName.trim().equals("helloWorld"))
+            return helloWorld();
+        else if (methodName.equals("printName"))
+            return printName();
+        else if (methodName.equals("sessionName"))
+            return sessionName();
+        return "Not a valid method";
+    }
 }
